58e8db20095528d43d17d475c0ab527f95ed8897
Jessi2704 <jessibrzapo@gmail.com>
2025-06-18 22:12:20 -0500

    Test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { appConfig } from './app.config';
+import { App } from './app';
+import { HelloWorld } from './pages/hello-world/hello-world';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HelloWorld
+    },
+    {
+        path:'**',
+        redirectTo:'home'
+    },
+    
+];
